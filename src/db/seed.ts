eb08c3e7cda133c60a3e211ae3dc136f6386ebf9
Lucas Henrique <lucashcolivera@gmail.com>
2024-09-12 21:48:54 -0300

    termino da construção do db

---
+++ src/db/seed.ts
@@ -0,0 +1,29 @@
+import { client, db } from '.'
+import { goalCompletions, goals } from './schema'
+import dayjs from 'dayjs'
+
+async function seed() {
+  await db.delete(goalCompletions)
+  await db.delete(goals)
+
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: 'Acordarcedo', desiredWeeklyFrequency: 6 },
+      { title: 'Me exercitar', desiredWeeklyFrequency: 6 },
+      { title: 'meditar', desiredWeeklyFrequency: 2 },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+    { goalId: result[2].id, createdAt: startOfWeek.add(3, 'days').toDate() },
+  ])
+}
+
+seed().finally(() => {
+  client.end()
+})
