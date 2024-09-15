bda3edfc1bb778f2949e159b6706271302aa5307
Lucas Henrique <lucashcolivera@gmail.com>
2024-09-15 09:00:29 -0300

    implementação de features para as goals e rotas

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,61 @@
 import fastfy from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import z from 'zod'
+import { createGoal } from '../services/create-goal'
+import { getWeekPendingGoals } from '../services/get-week-pending-goals'
+import { createGoalCompletion } from '../services/create-goal-completon'
 
-const app = fastfy()
+const app = fastfy().withTypeProvider<ZodTypeProvider>()
 
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return pendingGoals
+})
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
+app.post(
+  '/completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body
+
+    const result = await createGoalCompletion({
+      goalId,
+    })
+  }
+)
 app
   .listen({
     port: 3333,
